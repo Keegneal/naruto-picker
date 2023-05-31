85f038ac5d860c503ea63509f1628181d5a0c72d
Keegneal <92490783+Keegneal@users.noreply.github.com>
2023-05-31 16:13:37 -0500

    Add files via upload

---
+++ Naruto JS Project/main.js
@@ -0,0 +1,21 @@
+document.getElementById('naruto').onclick= narutoBro
+document.getElementById('itachi').onclick= itachiBro
+document.getElementById('madara').onclick= madaraBro
+document.getElementById('obito').onclick= obitoBro
+
+function narutoBro(){
+    document.querySelector('body').style.backgroundImage= "url('images/naruto.1.jpg')"
+
+}
+
+function itachiBro(){
+    document.querySelector('body').style.backgroundImage= "url('images/itachi.1.jpg')"
+}
+
+function madaraBro(){
+    document.querySelector('body').style.backgroundImage= "url('images/madara1.jpg')"
+}
+
+function obitoBro(){
+    document.querySelector('body').style.backgroundImage= "url('images/obito1.jpg')"
+}
